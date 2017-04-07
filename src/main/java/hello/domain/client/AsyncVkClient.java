9996219b8e1678a1db1bc8dcc05d99e257523964
Pavel Polushkin <Pavel.Polushkin@sidenis.com>
2017-04-07 15:35:30 +0300

    cf manifest added

--- src/main/java/hello/domain/client/AsyncVkClient.java
+++ src/main/java/hello/domain/client/AsyncVkClient.java
@@ -1,19 +1,16 @@
 package hello.domain.client;
 
-import com.sun.istack.internal.NotNull;
 import hello.domain.vo.VkUserData;
 
 import java.util.concurrent.CompletableFuture;
 
 /**
- *
  * Created by pavel on 21.09.16.
  */
 public interface AsyncVkClient {
 
     String URL = "https://api.vk.com/method/users.get?user_ids=%s&fields=photo_50,city,verified,sex,bdate,interests&v=5.8";
 
-    @NotNull
     CompletableFuture<VkUserData> getUserDataAsync(String vkId);
 
 }
--- src/main/java/hello/domain/client/VkClient.java
+++ src/main/java/hello/domain/client/VkClient.java
@@ -1,11 +1,8 @@
 package hello.domain.client;
 
-import com.sun.istack.internal.NotNull;
-import com.sun.istack.internal.Nullable;
 import hello.domain.vo.VkUserData;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CompletableFuture;
 
 /**
  * Created by pavel on 12.08.16.
@@ -15,7 +12,6 @@ public interface VkClient {
 
     String URL = "https://api.vk.com/method/users.get?user_ids=%s&fields=photo_50,city,verified,sex,bdate,interests&v=5.8";
 
-    @Nullable
     VkUserData getUserData(String vkId);
 
 
